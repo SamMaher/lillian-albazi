92c9fa65059e31e0ffa45a1aa4901bcb0005183c
SamMaher <samsmaher@gmail.com>
2021-06-09 21:50:01 +1000

    Added spotify link

--- src/components/socials-list/socials-urls.ts
+++ src/components/socials-list/socials-urls.ts
@@ -1,6 +1,6 @@
 const Websites = {
     Bandcamp: "https://lillianalbazi.bandcamp.com/",
-    Spotify: "",
+    Spotify: "https://distrokid.com/hyperfollow/lillianalbazi/after-image",
     Youtube: "https://www.youtube.com/watch?v=pNuAATWnuSU",
     Facebook: "https://www.facebook.com/Lillian-Albazi-231807707204525/",
     Instagram: "https://www.instagram.com/lalbazi/",
